a0d0b41e0ac8224b8255897beaf80aa9dbf72c05
Fekete János Dávid <janosdavidfekete@gmail.com>
2024-11-08 09:59:44 +0100

    Első óra

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BaseService } from './base.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AlieFrontend';
+  aliens:any
+
+  constructor(private base:BaseService) {
+    base.getAliens().subscribe(
+      (res) => this.aliens = res
+    )
+  }
 }
